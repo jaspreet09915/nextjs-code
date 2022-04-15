54c34a652e7b1c91aecf4d02546a9be8cf961025
jaspreet09915 <jaspreetsingh09915@gmail.com>
2022-04-15 16:06:56 +0530

    equality in javascript

---
+++ javascript refresher/05_equality.js
@@ -0,0 +1,15 @@
+// in javascript "==" it will first change its datatype and compare it
+if(1 == '1'){
+    console.log("It is true");
+}
+// it will when its value as well as its datatype is same
+if(1==="1"){
+    console.log("it is also true");
+}
+
+if(1=="1"){
+    console.log("it is true")
+}
+else{
+    console.log("it is false")
+}

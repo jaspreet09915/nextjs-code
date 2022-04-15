8b3d2be4bca00b2e15174838001f20a0e5afe625
jaspreet09915 <jaspreetsingh09915@gmail.com>
2022-04-15 15:06:23 +0530

    async func in javascript

---
+++ javascript refresher/02_async_func.js
@@ -0,0 +1,7 @@
+// javascript  is asynchronous in nature
+
+console.log("jaspreet is good boy");
+setTimeout(() => {
+    console.log("jaspreet is a herio! inside settimeout func");
+}, 1000); // timer in miniseconds 1000 means 1sec
+console.log("jaspreet is bad");

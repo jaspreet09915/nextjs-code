d684c352338a05ea39e02690985901001b7a9bd7
jaspreet09915 <jaspreetsingh09915@gmail.com>
2022-04-15 16:01:33 +0530

    map filter and reduce in javascript

---
+++ javascript refresher/04_map_filter_reduce.js
@@ -0,0 +1,28 @@
+
+let a = [1,2,3,4]
+// map  in javascript
+// let a2 = a.map((value)=>{
+//     return value*2;
+// })
+// console.log(a2);
+
+// filter in javascript
+// let a3 = a.filter((value)=>{
+//     return value >2;
+// })
+// console.log(a3);
+
+// reduce in javascript
+
+let a4 = a.reduce((val1, val2)=>{
+    return val1 + val2
+})
+console.log(a4);
+//starting arr value = 1,2,3,4
+// in a4
+// 1+2 , 3 , 4
+// 3 , 3 , 4
+// 3+3 , 4
+// 6 , 4
+// 6+4
+// 10 

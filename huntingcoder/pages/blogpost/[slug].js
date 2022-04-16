a33c958857bab4fc39fc71e9a83b2ade4bf1ef3e
jaspreet09915 <jaspreetsingh09915@gmail.com>
2022-04-17 02:17:44 +0530

    tut20 fetching blogpost using slug configure

--- huntingcoder/pages/blogpost/[slug].js
+++ huntingcoder/pages/blogpost/[slug].js
@@ -1,19 +1,32 @@
 import React from 'react'
 import { useRouter } from 'next/router' 
 import styles from '../../styles/BlogPost.module.css'
+import react , {useEffect,useState} from 'react'
 // dynamic route in nextjs
 // step 1: find the file corresponding to the slug
 // step2 : populate them inside the page
 const slug = () => {
-    const router = useRouter();
-    const {slug} = router.query;
+  const [blog, setBlog] = useState()
+  const router = useRouter();
+    useEffect(() => {
+      if(!router.isReady)return;
+      const {slug} = router.query;
+      fetch(`http://localhost:3000/api/getblogs?slug=${slug}`).then((a)=>{
+        return a.json();
+      })
+      .then((parsed)=>{
+        setBlog(parsed);
+      })
+    }, [router.isReady])
+    
+
   return (
     <div className={styles.container}>
      <main className={styles.main}>
         <div>
-          <h1>Title of the page {slug}</h1>
+          <h1> {blog && blog.title} </h1>
           <hr />
-          <h3>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Repellendus, architecto. Consequatur impedit, doloribus placeat quia ad consequuntur quo, sit commodi quaerat porro pariatur nihil! Odit ullam ratione quae consequatur, sunt facilis accusantium doloribus, libero modi sapiente voluptates, ipsa facere sint nobis suscipit id unde obcaecati! Vitae vero ex ullam similique autem. Cum, expedita! Ipsum?</h3>
+          <h3>{blog && blog.content}</h3>
       </div>
     </main>
   </div>

a9160bca1e8e12d728aaee25bcfb3194d0997971
jaspreet09915 <jaspreetsingh09915@gmail.com>
2022-04-19 02:46:13 +0530

    tut27 creating an about page

--- huntingcoder/pages/about.js
+++ huntingcoder/pages/about.js
@@ -1,8 +1,28 @@
 import React from 'react'
+import styles from '../styles/About.module.css'
 
 const About = () => {
   return (
-    <div>This is my about component</div>
+    <div className={styles.Container}>
+      <h1 className={styles.center}>About Hunting Coder</h1>
+      <h2>Introduction</h2>
+      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Commodi eaque est incidunt. Vitae, culpa quidem? Quaerat perferendis non ipsa aliquid minus rerum vel rem distinctio praesentium, illum soluta sint vo o repellat? Quia a corporis recusandae, consequuntur, voluptates ducimus maiores architecto repudiandae incidunt. Ad, esse nulla dolorum i deserunt expedita omnis vitae. Nulla quidem sed dolor doloremque aliquid porro sint saepe sequi repudiandae molestiae sapiente consequuntur quisquam, blanditiis fugit unde accusantium et commodi, explicabo, distinctio excepturi. Libero quibusdam consequuntur deleniti fuga laborum.</p>
+    <h2>Service Offered!</h2>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempora corrupti dolorum, maiores saepe quae explicabo! Saepe ab vitae temporibus quod nostrum animi quaerat totam, ex dolore numquam fugit delectus, impedit odit debitis deleniti cupiditate.</p>
+    <p>We offer the following services</p>
+    <ul>
+      <li>service 1</li>
+      <li>service 2</li>
+      <li>service 3</li>
+      <li>service 4</li>
+      <li>service 5</li>
+      <li>service 6</li>
+      <li>service 7</li>
+    </ul>
+    <h2>Contact Us</h2>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quibusdam ad ab nobis molestias itaque error corporis vero placeat provident, dolor illo, consectetur veniam, dolorem minus voluptate. Nisi aliquam nam culpa quasi, quaerat laudantium suscipit.</p>
+    </div>
+
   )
 }
 

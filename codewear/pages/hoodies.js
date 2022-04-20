85e5b2b2a3c9d63619e874720d587070d50ee06a
jaspreet09915 <jaspreetsingh09915@gmail.com>
2022-04-21 03:15:42 +0530

    tut38 Designing hoodies, mugs, stickers pages

--- codewear/pages/hoodies.js
+++ codewear/pages/hoodies.js
@@ -1,8 +1,182 @@
 import React from 'react'
-
+import Link from "next/link";
+ 
 const Hoodies = () => {
   return (
-    <div>Hoodies</div>
+    <div>      <section className="text-gray-600 body-font">
+    <div className="container px-5 py-24 mx-auto">
+      <div className="flex flex-wrap -m-4">
+        <Link href={"/product/wear-the-code"}>
+          <div className="lg:w-1/5 md:w-1/2 p-4 w-full cursor-pointer shadow-lg m-5">
+            <a className="block relative  rounded overflow-hidden">
+              <img
+                alt="ecommerce"
+                className="h-[30vh] md:h-[36vh] block m-auto"
+                src="hoodies.jpg"
+              />
+            </a>
+            <div className="mt-4 text-center">
+              <h3 className="text-gray-500 text-xs tracking-widest title-font mb-1">
+                T-shirts
+              </h3>
+              <h2 className="text-gray-900 title-font text-lg font-medium">
+                Wear the code
+              </h2>
+              <p className="mt-1">₹165.00</p>
+              <p className="mt-1">S, M, L, XL, XXL</p>
+            </div>
+          </div>
+        </Link>
+        <Link href={"/product/wear-the-code"}>
+          <div className="lg:w-1/5 md:w-1/2 p-4 w-full cursor-pointer shadow-lg m-5">
+            <a className="block relative  rounded overflow-hidden">
+              <img
+                alt="ecommerce"
+                className="h-[30vh] md:h-[36vh] block m-auto"
+                src="hoodies.jpg"
+              />
+            </a>
+            <div className="mt-4 text-center">
+              <h3 className="text-gray-500 text-xs tracking-widest title-font mb-1">
+                T-shirts
+              </h3>
+              <h2 className="text-gray-900 title-font text-lg font-medium">
+                Wear the code
+              </h2>
+              <p className="mt-1">₹215.15</p>
+              <p className="mt-1">S, M, L, XL, XXL</p>
+            </div>
+          </div>
+        </Link>
+        <Link href={"/product/wear-the-code"}>
+          <div className="lg:w-1/5 md:w-1/2 p-4 w-full cursor-pointer shadow-lg m-5">
+            <a className="block relative  rounded overflow-hidden">
+              <img
+                alt="ecommerce"
+                className="h-[30vh] md:h-[36vh] block m-auto"
+                src="hoodies.jpg"
+              />
+            </a>
+            <div className="mt-4 text-center">
+              <h3 className="text-gray-500 text-xs tracking-widest title-font mb-1">
+                T-shirts
+              </h3>
+              <h2 className="text-gray-900 title-font text-lg font-medium">
+                Wear the code
+              </h2>
+              <p className="mt-1">₹125.00</p>
+              <p className="mt-1">S, M, L, XL, XXL</p>
+            </div>
+          </div>
+        </Link>
+        <Link href={"/product/wear-the-code"}>
+          <div className="lg:w-1/5 md:w-1/2 p-4 w-full cursor-pointer shadow-lg m-5">
+            <a className="block relative  rounded overflow-hidden">
+              <img
+                alt="ecommerce"
+                className=" h-[30vh] md:h-[36vh] block m-auto"
+                src="hoodies.jpg"
+              />
+            </a>
+            <div className="mt-4 text-center">
+              <h3 className="text-gray-500 text-xs tracking-widest title-font mb-1">
+                T-shirts
+              </h3>
+              <h2 className="text-gray-900 title-font text-lg font-medium">
+                Wear the code
+              </h2>
+              <p className="mt-1">₹185.40</p>
+              <p className="mt-1">S, M, L, XL, XXL</p>
+            </div>
+          </div>
+        </Link>
+        <Link href={"/product/wear-the-code"}>
+          <div className="lg:w-1/5 md:w-1/2 p-4 w-full cursor-pointer shadow-lg m-5">
+            <a className="block relative  rounded overflow-hidden">
+              <img
+                alt="ecommerce"
+                className="h-[30vh] md:h-[36vh] block m-auto"
+                src="hoodies.jpg"
+              />
+            </a>
+            <div className="mt-4 text-center">
+              <h3 className="text-gray-500 text-xs tracking-widest title-font mb-1">
+                T-shirts
+              </h3>
+              <h2 className="text-gray-900 title-font text-lg font-medium">
+                Wear the code
+              </h2>
+              <p className="mt-1">₹165.00</p>
+              <p className="mt-1">S, M, L, XL, XXL</p>
+            </div>
+          </div>
+        </Link>
+        <Link href={"/product/wear-the-code"}>
+          <div className="lg:w-1/5 md:w-1/2 p-4 w-full cursor-pointer shadow-lg m-5">
+            <a className="block relative  rounded overflow-hidden">
+              <img
+                alt="ecommerce"
+                className="h-[30vh] md:h-[36vh] block m-auto"
+                src="hoodies.jpg"
+              />
+            </a>
+            <div className="mt-4 text-center">
+              <h3 className="text-gray-500 text-xs tracking-widest title-font mb-1">
+                T-shirts
+              </h3>
+              <h2 className="text-gray-900 title-font text-lg font-medium">
+                Wear the code
+              </h2>
+              <p className="mt-1">₹215.15</p>
+              <p className="mt-1">S, M, L, XL, XXL</p>
+            </div>
+          </div>
+        </Link>
+        <Link href={"/product/wear-the-code"}>
+          <div className="lg:w-1/5 md:w-1/2 p-4 w-full cursor-pointer shadow-lg m-5">
+            <a className="block relative  rounded overflow-hidden">
+              <img
+                alt="ecommerce"
+                className="h-[30vh] md:h-[36vh] block m-auto"
+                src="hoodies.jpg"
+              />
+            </a>
+            <div className="mt-4 text-center">
+              <h3 className="text-gray-500 text-xs tracking-widest title-font mb-1">
+                T-shirts
+              </h3>
+              <h2 className="text-gray-900 title-font text-lg font-medium">
+                Wear the code
+              </h2>
+              <p className="mt-1">₹125.00</p>
+              <p className="mt-1">S, M, L, XL, XXL</p>
+            </div>
+          </div>
+        </Link>
+        <Link href={"/product/wear-the-code"}>
+          <div className="lg:w-1/5 md:w-1/2 p-4 w-full cursor-pointer shadow-lg m-5">
+            <a className="block relative  rounded overflow-hidden">
+              <img
+                alt="ecommerce"
+                className="h-[30vh] md:h-[36vh] block m-auto"
+                src="hoodies.jpg"
+              />
+            </a>
+            <div className="mt-4 text-center">
+              <h3 className="text-gray-500 text-xs tracking-widest title-font mb-1">
+                T-shirts
+              </h3>
+              <h2 className="text-gray-900 title-font text-lg font-medium">
+                Wear the code
+              </h2>
+              <p className="mt-1">₹185.40</p>
+              <p className="mt-1">S, M, L, XL, XXL</p>
+            </div>
+          </div>
+        </Link>
+      </div>
+    </div>
+  </section></div>
   )
 }
 
